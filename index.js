15eb578e9f249a94aec74f2c49cbe27b4112498d
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2016-09-21 18:48:33 +0200

    upgraded to es6 and babel

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-var Beacon = require('beacon');
+import { firebase } from 'devices-core-firebase';
+import { beacon } from 'beacon';
+// var beacon = require('beacon')
+const TEAM = 'my-awsome-team-name';
+const fb = new firebase(TEAM);
 
 //This is used to create a fake beacon
-Beacon.startAdvertising('e2c56db5dffb48d2b060d0f5a71096e0', 0, 0, -59);
+beacon.startAdvertising('e2c56db5dffb48d2b060d0f5a71096e0', 0, 0, -59);
 
 //This is to detect beacons in general
-Beacon.on('discover', function(Beacon) {
+beacon.on('discover', function(Beacon) {
   console.log('Beacon found: ' + JSON.stringify(Beacon));
 });
 
 //This is to detect more detailed data from the bigger beacons
-Beacon.on('telemetrydata', function(telemetrydata){
+beacon.on('telemetrydata', function(telemetrydata){
 	console.log('telemetrydata: ', telemetrydata);
 });
 
 //Here you can either scan for everything, or a specific ID
-Beacon.startScanning(/*'e2c56db5dffb48d2b060d0f5a71096e0', 0, 0*/);
+beacon.startScanning(/*'e2c56db5dffb48d2b060d0f5a71096e0', 0, 0*/);
