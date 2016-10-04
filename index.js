6b9c490364f658ed82dbbb43956b896d6adac26d
Lars Eliassen <lars.eliassen@bekk.no>
2016-10-04 19:09:55 +0200

    Endrer minor på advertiset beacon

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import { firebase } from 'devices-core-firebase';
 import { beacon } from 'beacon';
-const TEAM = 'my-awsome-team-name';
+const TEAM = 'RÆVSNÆRK';
 const fb = new firebase(TEAM);
 
 //Incorporated this library: https://github.com/sandeepmistry/node-bleacon
 
 //This is used to create a fake beacon, with id, major, minor, measuredPower
-beacon.startAdvertising('e2c56db5dffb48d2b060d0f5a71096e0', 0, 0, -59);
+beacon.startAdvertising('e2c56db5dffb48d2b060d0f5a71096e0', 0, 10, -59);
 
 //This is to detect beacons in general
-beacon.on('discover', function(Beacon) {
+beacon.on('discover', function(Beacon) {    
   console.log('Beacon found: ' + JSON.stringify(Beacon));
 });
 
