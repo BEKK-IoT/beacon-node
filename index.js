33144fb9e1643455bf69db6f2524910407b604aa
Birgitte Haavardsholm <birgitte.haavardsholm@gmail.com>
2016-10-05 14:49:16 +0200

    fjernet eksempelkode

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 import { firebase } from 'devices-core-firebase';
 import { beacon } from 'beacon';
-const TEAM = 'my-awesome-team-name';
+import fetch from 'node-fetch';
+
+const TEAM = 'my-awsome-team-name';
 const fb = new firebase(TEAM);
-
-//Incorporated this library: https://github.com/sandeepmistry/node-bleacon
-
-//This is used to create a fake beacon, with id, major, minor, measuredPower
-//beacon.startAdvertising('e2c56db5dffb48d2b060d0f5a71096e0', 0, 10, -59);
-
-//This is to detect beacons in general
-beacon.on('discover', function(Beacon) {    
-  console.log('Beacon found: ' + JSON.stringify(Beacon));
-});
-
-//This is to detect more detailed data from the bigger beacons
-beacon.on('telemetrydata', function(telemetrydata){
-	console.log('telemetrydata: ', telemetrydata);
-});
-
-//Here you can either scan for everything, or a specific ID, and major and minor
-beacon.startScanning(/*'e2c56db5dffb48d2b060d0f5a71096e0', 0, 0*/);
